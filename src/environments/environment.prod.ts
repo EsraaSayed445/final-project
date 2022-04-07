1675896f0aab2cc54d7f34d0aea7e9980829b006
EsraaSayed <esraasyed445@gmail.com>
2022-04-08 00:36:12 +0200

    laravel local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl:"http://localhost:8000/api/",
-  baseUrl:"https://restaurant-ang.herokuapp.com/api/"
+  baseUrl:"http://localhost:8000/api/",
+  // baseUrl:"https://restaurant-ang.herokuapp.com/api/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // baseUrl:"http://localhost:8000/api/",
-  baseUrl:"https://restaurant-ang.herokuapp.com/api/"
+  baseUrl:"http://localhost:8000/api/",
+  // baseUrl:"https://restaurant-ang.herokuapp.com/api/"
 };
 
 /*
